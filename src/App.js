8bf70c4ba268943d664e6990f4d62aee3d723b46
IvanLopez <oscari.lopez@potros.itson.edu.mx>
2020-01-06 20:53:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Error from "./components/Error";
+import Clima from './components/Clima';
 
 function App() {
+  //State principal
+  //ciudad = state, guardarCiudad = setState();
+  const [ciudad, guardarCiudad] = useState("");
+  const [pais, guardarPais] = useState("");
+  const [error, guardarError] = useState(false);
+  const [resultado, guardarResulado] = useState({});
+
+  //Suplente de los metodos de ciclo de vida como: componentDidMount()
+  useEffect(() => {
+    //prevenir la ejecucion
+    if (ciudad === "") {
+      return;
+    }
+
+
+    const consultarAPI = async () => {
+      const appId = "eec494b710ee55ffd01d6002009d8280";
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+      //Consultar la URL
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+
+      guardarResulado(resultado);
+    };
+
+
+    consultarAPI();
+
+
+  }, [ciudad, pais]);
+
+  const datosConsulta = datos => {
+    //Validar que ambos campos esten
+    if (datos.ciudad === "" || datos.pais === "") {
+      //un error
+      guardarError(true);
+      return;
+    }
+
+    //Ciudad y pais existen, agregarlos al state
+    guardarCiudad(datos.ciudad);
+    guardarPais(datos.pais);
+    guardarError(false);
+  };
+
+  //Cargar un componente Condicionalmente
+  let componente;
+  if (error) {
+    //hay un error, mostrarlo
+    componente = <Error mensaje="Ambos campos son obligatorios" />;
+  }else if(resultado.cod === "404"){
+    componente = <Error mensaje="La ciudad no existe en nuestro registro"/>
+  }else {
+    //Mostrar el clima
+    componente = <Clima resultado = {resultado}
+    />;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header titulo="Clima React App" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario datosConsulta={datosConsulta} />
+            </div>
+
+            <div className="col s12 m6">{componente}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
